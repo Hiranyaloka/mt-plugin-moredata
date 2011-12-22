69a91d5d9b1e597ddbc34e1eea522da6bbba5b64
Rick Bychowski <wrinkles@gmail.com>
2011-12-21 22:01:52 -0800

    initial commit

---
+++ README.markdown
@@ -0,0 +1,138 @@
+# MoreData 0.14 for Movable Type 4 and Melody #
+
+MoreData parses finds and parses CSV strings from any Movable Type tag into a hash or array which can be captured as an MT variable. All with the single tag modifier, `moredata`.
+   
+## Embedding the data in a text field ##
+
+First embed a string anywhere within a text field accessible from an MT function tag. Let's use the EntryExcerpt field:
+
+    Excerpt
+    Here is my excerpt which I can output without the data.
+    
+    ---first_name=
+    Moe,Larry,Curly
+    
+    ---last_name=
+    Moe=>Howard,
+    Curly=>Howard,
+    Larry=>Fine
+    
+    ---say_yes=
+    Why, certainly!
+    ...
+    
+    And here is a continuation of the excerpt.
+    
+In your template, you can set an MT `string`, `array` or `hash` variable corresponding to one of the named strings (e.g. `say_yes` string, `first_name` array, `last_name` hash).
+
+The `moredata` modifier accepts one or two arguments: The data identifier is the first argument, and an optional second, th format: `array`, `hash`, or `string`). 
+If no format is given, the blog default is used. You capture the data using the `setvar = whatever` argument.
+
+You should review the Movable Type or Melody documentation of the `mt:Var` tag with arrays and hashes.
+
+## Setting the variables ##
+
+    <mt:EntryExcerpt convert_breaks="0" moredata="first_name","array" setvar="first_name_a">
+    <mt:EntryExcerpt convert_breaks="0" moredata="last_name","hash" setvar="last_name_h">
+    <mt:EntryExcerpt convert_breaks="0" moredata="say_yes","string" setvar="say_yes_s">
+
+__IMPORTANT:__ The `convert_breaks="0"` filter is important and should usually precede the `moredata` tag. This filter ensures that no extra formatting is inserted into your data by MT. If the text filter is set to `none` then you may not need to state explicitly.
+
+## Accessing the variables ##
+Array via loop:
+    <mt:Loop name="first_name_a">
+      <mt:Var name="__value__">,   # gives Moe, Larry, Curly
+    </mt:Loop>
+
+Array by index:
+    <mt:Var name="first_name_a[2]"> # gives Larry
+
+Hash by loop:
+    <mt:Loop name="last_name_h">
+      <mt:Var name="__key__"> <mt:Var name="__value__">, # gives Moe Howard, Curly Howard, Lary Fine
+    </mt:Loop>                                           # hash loops have their own special order
+
+Hash by key:
+    <mt:Var name="last_name_h{Moe}">  # gives Howard
+
+The array index or hash key can be a variable. Here is a slightly sophisticated example, in which I loop through an array, using the array value as the key to a different hash variable.
+
+    <mt:Loop name="first_name_a">
+      <mt:Var name="first_name_a"> says, "I'm Dr. <mt:Var name="last_name_h{$first_name_a}">!<br />
+    </mt:Loop>
+    <br />
+    Can we help you? <mt:Var name="say_yes_s"><br />
+
+
+## Retrieving the tag content without the data ##
+The content can be output separately with the `__content__` key:
+
+    My content is: <mt:EntryExcerpt moredata="__content__">
+    
+Will output 
+
+>> Here is my excerpt which I can output without the data. And here is a continuation of the excerpt.
+
+For debugging or whatnot, the `__data__` key outputs the complete raw data string:
+
+    My data is: <mt:EntryExcerpt convert_breaks="0" moredata="__data__">
+
+This produces the full data string, including opentags, but without the close tag.
+
+## Blog-wide plugin configuration ##
+
+The plugin takes five blog-wide settings:
+
+- `opentag` should be a unique string which opens a data section, and is required for each data identifier.
+- `closetag` is required at the end of the whole dataset. Optionally it can close each data section.
+- `datasep` is a string that joins items in an array, and key-value pairs.
+- `hashsep` is a string that joins keys from values.
+- `format` is the default format, used when a second argument to the `moredata` modifier is not given.
+
+## Formatting the data ##
+
+Each data section begins with an identifier, followed immediately by the data identifier and an equals sign:
+
+    ---my_data=
+    
+
+Each named dataset must be terminated by another named dataset, or a closetag if it is the last, or by the end of the file:
+
+    ---first=
+    one,two,three
+    ---second=
+    snow=>white,ruby=>red
+    ...
+    
+You can omit the close tag if the data is at the end of the content.
+  
+The named data sets must be in one contiguous block, and only one block is allowed.
+(Everything between the first `opentag` and the `closetag` (or eof) is considered data).
+You can put that data block in the middle of the content (but then don't forget the close tag).
+
+## Choosing tags and separators ##
+You can configure the open and close tags and the data and hash separator strings. The data separator and hash separator strings can be the same if you wish.
+You need to be wary of potential conflicts between your open and close tags, the separators, and your content or data.
+
+- Avoid having your open tags appearing in the preceding content or your close tags appearing in subsequent content.
+- You don't want your data separators (`,`, `=>` etc) to appear in your data, obviously.
+- You data identifiers can have spaces like `---first name=` or be empty `---=`
+
+In the former you would use `moredata="first name"`. The latter would be `moredata=""`. But don't use the bareword `moredata` without at least one argument, even if it is the empty string.
+
+##  RELATED PLUGINS ##
+The venerable and awesome [Key Values plugin by Brad Choate](http://bradchoate.com/weblog/2002/07/27/keyvalues) was my inspiration for the MoreData plugin.
+
+## SUPPORT DISCLAIMER ##
+This plugin is offered as is with no 
+
+## COPYRIGHT AND LICENSE ##
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+MoreData is Copyright 2011, Rick Bychowski, rick@hiranyaloka.com.
+All rights reserved.
---
+++ plugins/MoreData/lib/MoreData/Plugin.pm
@@ -0,0 +1,170 @@
+# Copyright 2011 Rick Bychowski
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of either: the GNU General Public License as published
+# by the Free Software Foundation; or the Artistic License.
+# See http://dev.perl.org/licenses/ for more information.
+
+package MoreData::Plugin;
+use MT 4.2;
+use warnings;
+use strict;
+use MT::Util qw(decode_html);
+
+sub moredata {
+  my ($str, $val, $ctx) = @_; # val is the data name and optional format (or default format)
+  $str = decode_html($str);
+  my $dataname; # retrieved from $val
+  my $content;  # extracted from $str
+  my $datastring; # extracted from $str
+  my $result; # processed from $datastring
+# retrieve configuration defaults
+  my $config = _moredata_config($ctx);
+# open tag is required
+  my $opentag = $config->{'moredata_opentag'} ? $config->{'moredata_opentag'} : '';
+  die "MoreData requires an open tag: $!" unless ($opentag);
+  my $closetag = $config->{'moredata_closetag'} ? $config->{'moredata_closetag'} : ''; # not required
+# retrieve format and data separator from tag parameters
+  my $format_cfg = $config->{'moredata_format'};
+  my $format = '';
+  if (ref($val) eq 'ARRAY') {
+    $dataname = defined($val->[0]) ? $val->[0] : '' ; #dataname can be empty
+    $format = $val->[1] ? ($val->[1]) : $format_cfg ;
+  } else { 
+    $dataname = $val ? $val : '';
+    $format = $format_cfg
+  }
+  die "No format specified in args or configuration: $!" unless $format;
+# extract datastrings
+  my $substrings_aref = _retrieve_strings($str, $opentag, $closetag, $dataname);
+  unless (scalar($substrings_aref)) { # no stringrefs returned
+    die "cannot extract substrings from string: $!";
+  }
+  $content = _trim($$substrings_aref[0]);
+  return $content if $dataname eq "__content__";
+  $datastring = _trim($$substrings_aref[1]);
+  return $datastring  if ($dataname eq '__data__'); # return all data as string
+  return $datastring unless (length($datastring)); # no sense in processing an empty string
+  my $datasep_cfg = $config->{'moredata_datasep'};
+  length($datasep_cfg) or die "The data separation string must be configured: $!";
+  my $hashsep_cfg = $config->{'moredata_hashsep'} || '';
+  length($hashsep_cfg) or die "The hash separation string must be configured: $!";
+# send data and parameters to desired format for result
+  if ($format eq 'array') {
+    $result = _moredata_array($datastring, $datasep_cfg);
+  } elsif  ($format eq 'hash') {
+    $result = _moredata_hash($datastring, $datasep_cfg, $hashsep_cfg);
+  } elsif  ($format eq 'string') {
+    $result = $datastring;
+  } else {
+    die "no format : $!";
+  }
+  return $result;
+}
+
+# csv routine expects datastring ref and array ref of arguments
+sub _moredata_array {
+  my ($datastring, $datasep_cfg) = @_;
+  my @list = split /\s*$datasep_cfg\s*/, $datastring;
+  scalar @list ? \@list : undef;
+}
+
+sub _moredata_hash {
+  my ($datastring, $datasep_cfg, $hashsep_cfg) = @_;
+  my $datasep = _escape($datasep_cfg);
+  my $hashsep = _escape($hashsep_cfg);
+  my %hash;
+  my @list = split /\s*$datasep\s*/, $datastring;
+  foreach my $item (@list) {
+    my $count = my @array = split(/\s*$hashsep\s*/, $item);
+    die "Count of $count is not a pair of values in hash assignment of values @array : $!" if ($count != (2 || 0)); 
+    $hash{$array[0]} = $array[1];
+  }
+  scalar %hash ? \%hash : undef;
+}
+
+sub _moredata_config {
+  my $ctx = shift;
+  my $plugin = MT->component("MoreData");
+  my $blog = $ctx->stash('blog');
+  if ( !$blog ) {
+      my $blog_id = $ctx->var('blog_id');
+      $blog = MT->model('blog')->load($blog_id);
+  }
+  my $blog_id = $blog->id;
+  my $scope = "blog:" . $blog_id;
+  my $config = $plugin->get_config_hash($scope);
+  return $config;
+}
+
+# return content and data strings from string and tags
+sub _retrieve_strings {
+  my ($str, $opentag, $closetag, $dataname) = @_;
+  my $content = $str;
+  my $datastring = '';
+# content is all non-data, datastring all data
+  my @substrings = ($content, $datastring);
+  die "MoreData requires an open tag in plugin configuration: $!" unless (length($opentag));
+# check that we have a string with length.
+  my $stringlength = length($str);
+  return \@substrings unless ($stringlength);   
+# return unless opentag is found in string
+  my $openposition = index($str,$opentag); # start of _all_ the data
+  return \@substrings if ($openposition == -1);
+# close tag cannot be subset of open tag
+  die "MoreData close tag cannot be substring of the open tag: $!"
+    unless (index($opentag,$closetag) == -1);
+# extract the content and data strings
+  my $closeposition; # end of _all_ the data
+  if ((length($closetag)) && (rindex($str,$closetag) != -1)) { # closetag found
+    $closeposition = index($str,$closetag);
+  } else {
+    $closeposition = $stringlength;
+  }
+  my $datalength = $closeposition - $openposition; # length of _all_ data
+  die "MoreData open tag must precede the close tag: $!" if ($datalength < 0); #tags in wrong order;
+  $content = substr($str, 0, $openposition) . substr($str, ($closeposition + length($closetag)), $stringlength); #length argument can be beyond the end
+# data includes open tag but not close tag
+  $datastring = substr($str, $openposition, $datalength); # _all_ data
+  @substrings = ($content, $datastring);
+  return \@substrings if ($dataname eq '__data__'); # return content and data as strings
+  return \@substrings if ($dataname eq '__content__'); # return content and data as strings
+  return \@substrings unless length($datastring); # no reason to process empty datastring
+# search for named daatastring
+  my $nameopentag = $opentag . $dataname . '='; # selected data requires the equal sign appended
+# locate the desired selected data within the datastring
+  my $nameopenposition = index($datastring, $nameopentag);
+  if ($nameopenposition == -1) { # if named data can't be found, return substrings with empty datastring
+    @substrings = ($content, '');
+    return \@substrings;
+  }
+  my $length_tagname = length($nameopentag);
+  my $namecloseposition;
+  if (index($datastring, $opentag, $nameopenposition + 1) != -1 ) {  # if we find another opentag following current one
+    $namecloseposition = index($datastring, $opentag, $nameopenposition + 1); # find next opentag
+  } elsif (index($datastring, $closetag, $nameopenposition + 1) != -1 ) {  # otherwise use closing tag
+    $namecloseposition = index($datastring, $closetag, $nameopenposition + 1);
+  } else {  # else just use end of file
+    $namecloseposition = $datalength;
+  }
+  $datalength = $namecloseposition - $nameopenposition - $length_tagname;
+  my $datastring = substr($datastring, $nameopenposition + $length_tagname, $datalength); 
+  @substrings = ($content, $datastring);
+  return \@substrings;
+}
+
+sub _escape {
+my $separator = shift;
+my $regex = '\\' . join('\\', split(/''/, $separator) );
+return $regex;
+}
+
+# Perl trim function to remove whitespace from the start and end of the string
+sub _trim {
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
+
+
+1;
