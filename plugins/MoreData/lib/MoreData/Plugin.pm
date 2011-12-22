e23a479e0c1cc10943e5734f9955c76b693585a8
Rick Bychowski <wrinkles@gmail.com>
2011-12-22 09:28:19 -0800

    code cleanup - thanks to jwkrahn at perlmonks.org

--- plugins/MoreData/lib/MoreData/Plugin.pm
+++ plugins/MoreData/lib/MoreData/Plugin.pm
@@ -20,34 +20,30 @@ sub moredata {
 # retrieve configuration defaults
   my $config = _moredata_config($ctx);
 # open tag is required
-  my $opentag = $config->{'moredata_opentag'} ? $config->{'moredata_opentag'} : '';
-  die "MoreData requires an open tag: $!" unless ($opentag);
-  my $closetag = $config->{'moredata_closetag'} ? $config->{'moredata_closetag'} : ''; # not required
+  my $opentag = $config->{ moredata_opentag } or die "MoreData requires an open tag\n";
+  my $closetag = $config->{ moredata_closetag } || ''; # not required
 # retrieve format and data separator from tag parameters
   my $format_cfg = $config->{'moredata_format'};
   my $format = '';
   if (ref($val) eq 'ARRAY') {
-    $dataname = defined($val->[0]) ? $val->[0] : '' ; #dataname can be empty
-    $format = $val->[1] ? ($val->[1]) : $format_cfg ;
+    $dataname = @$val ? $val->[0] : '' ; #dataname can be empty
+    $format = $val->[1] || $format_cfg ;
   } else { 
-    $dataname = $val ? $val : '';
+    $dataname = $val || '';
     $format = $format_cfg
   }
-  die "No format specified in args or configuration: $!" unless $format;
+  die "No format specified in args or configuration" unless $format;
 # extract datastrings
-  my $substrings_aref = _retrieve_strings($str, $opentag, $closetag, $dataname);
-  unless (scalar($substrings_aref)) { # no stringrefs returned
-    die "cannot extract substrings from string: $!";
-  }
+  my $substrings_aref = _retrieve_strings($str, $opentag, $closetag, $dataname) or die "cannot extract substrings from string\n";
   $content = _trim($$substrings_aref[0]);
   return $content if $dataname eq "__content__";
   $datastring = _trim($$substrings_aref[1]);
   return $datastring  if ($dataname eq '__data__'); # return all data as string
   return $datastring unless (length($datastring)); # no sense in processing an empty string
   my $datasep_cfg = $config->{'moredata_datasep'};
-  length($datasep_cfg) or die "The data separation string must be configured: $!";
+  length($datasep_cfg) or die "The data separation string must be configured\n";
   my $hashsep_cfg = $config->{'moredata_hashsep'} || '';
-  length($hashsep_cfg) or die "The hash separation string must be configured: $!";
+  length($hashsep_cfg) or die "The hash separation string must be configured\n";
 # send data and parameters to desired format for result
   if ($format eq 'array') {
     $result = _moredata_array($datastring, $datasep_cfg);
@@ -67,20 +63,18 @@ sub _moredata_array {
   scalar @list ? \@list : undef;
 }
 
-sub _moredata_hash {
-  my ($datastring, $datasep_cfg, $hashsep_cfg) = @_;
-  my $datasep = _escape($datasep_cfg);
-  my $hashsep = _escape($hashsep_cfg);
-  my %hash;
-  my @list = split /\s*$datasep\s*/, $datastring;
-  foreach my $item (@list) {
-    my $count = my @array = split(/\s*$hashsep\s*/, $item);
-    die "Count of $count is not a pair of values in hash assignment of values @array : $!" if ($count != (2 || 0)); 
-    $hash{$array[0]} = $array[1];
-  }
-  scalar %hash ? \%hash : undef;
-}
-
+ sub _moredata_hash {
+   my ($datastring, $datasep, $hashsep) = @_;
+   my %hash;
+   my @list = split /\s*\Q$datasep\E\s*/, $datastring;
+   foreach my $item (@list) {
+     my $count = my @array = split(/\s*\Q$hashsep\E\s*/, $item);
+     die "Count of $count is not a pair of values in hash assignment of values @array : $!" if ($count != (2 || 0)); 
+     $hash{$array[0]} = $array[1];
+   }
+   scalar %hash ? \%hash : undef;
+ }
+ 
 sub _moredata_config {
   my $ctx = shift;
   my $plugin = MT->component("MoreData");
@@ -101,14 +95,13 @@ sub _retrieve_strings {
   my $content = $str;
   my $datastring = '';
 # content is all non-data, datastring all data
-  my @substrings = ($content, $datastring);
   die "MoreData requires an open tag in plugin configuration: $!" unless (length($opentag));
 # check that we have a string with length.
   my $stringlength = length($str);
-  return \@substrings unless ($stringlength);   
+  return [$content, $datastring] unless ($stringlength);   
 # return unless opentag is found in string
   my $openposition = index($str,$opentag); # start of _all_ the data
-  return \@substrings if ($openposition == -1);
+  return [$content, $datastring] if ($openposition == -1);
 # close tag cannot be subset of open tag
   die "MoreData close tag cannot be substring of the open tag: $!"
     unless (index($opentag,$closetag) == -1);
@@ -124,17 +117,15 @@ sub _retrieve_strings {
   $content = substr($str, 0, $openposition) . substr($str, ($closeposition + length($closetag)), $stringlength); #length argument can be beyond the end
 # data includes open tag but not close tag
   $datastring = substr($str, $openposition, $datalength); # _all_ data
-  @substrings = ($content, $datastring);
-  return \@substrings if ($dataname eq '__data__'); # return content and data as strings
-  return \@substrings if ($dataname eq '__content__'); # return content and data as strings
-  return \@substrings unless length($datastring); # no reason to process empty datastring
+  return [$content, $datastring] if ($dataname eq '__data__'); # return content and data as strings
+  return [$content, $datastring] if ($dataname eq '__content__'); # return content and data as strings
+  return [$content, $datastring] unless length($datastring); # no reason to process empty datastring
 # search for named daatastring
   my $nameopentag = $opentag . $dataname . '='; # selected data requires the equal sign appended
 # locate the desired selected data within the datastring
   my $nameopenposition = index($datastring, $nameopentag);
   if ($nameopenposition == -1) { # if named data can't be found, return substrings with empty datastring
-    @substrings = ($content, '');
-    return \@substrings;
+    return [$content, ''];
   }
   my $length_tagname = length($nameopentag);
   my $namecloseposition;
@@ -147,14 +138,7 @@ sub _retrieve_strings {
   }
   $datalength = $namecloseposition - $nameopenposition - $length_tagname;
   my $datastring = substr($datastring, $nameopenposition + $length_tagname, $datalength); 
-  @substrings = ($content, $datastring);
-  return \@substrings;
-}
-
-sub _escape {
-my $separator = shift;
-my $regex = '\\' . join('\\', split(/''/, $separator) );
-return $regex;
+  return [$content, $datastring];
 }
 
 # Perl trim function to remove whitespace from the start and end of the string
